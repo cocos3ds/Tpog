2a21d35735445c3114927fba33aefd085a888f43
neo <1281105606@qq.com>
2018-03-15 10:24:43 +0800

    完成用户登录功能

--- application/admin/controller/Login.php
+++ application/admin/controller/Login.php
@@ -3,6 +3,7 @@
 namespace app\admin\controller;
 
 use think\Controller;
+use think\Db;
 use think\Request;
 
 class Login extends Controller{
@@ -14,11 +15,26 @@ class Login extends Controller{
     public function check(){
 
         $code = $this->request->param('code');
+        $username = $this->request->param('username');
+        $password = $this->request->param('password');
 
+        if(empty($username)){
+            $this->error('用户名不能为空');
+        }
+        if(empty($password)){
+            $this->error('密码不能为空');
+        }
         if(!captcha_check($code)){
             $this->error('验证码错误');
-        }else{
-            $this->success('验证码正确');
         }
+
+        $result = Db::table('user')->where('username',$username)->where('password',md5('tp'.$password))->find();
+
+        if($result != null){
+            session('username',$username);
+            $this->success('登录成功',url('admin/index/index'));
+        }
+
+
     }
 }
--- application/admin/controller/Register.php
+++ application/admin/controller/Register.php
@@ -3,6 +3,7 @@
 namespace app\admin\controller;
 
 use think\Controller;
+use think\Db;
 use think\Request;
 
 class Register extends Controller
@@ -20,10 +21,47 @@ class Register extends Controller
 
     public function check(){
         $code = $this->request->param('code');
+        $username = $this->request->param('username');
+        $password1 = $this->request->param('password1');
+        $password2 = $this->request->param('password2');
+        if(empty($username)){
+            $this->error('用户名不能为空');
+        }
+        if(empty($password1)){
+            $this->error('密码不能为空');
+        }
+
+        if(empty($password2)){
+            $this->error('确认密码不能为空');
+        }
+
         if(!captcha_check($code)){
             $this->error('验证码错误');
-            exit();
-        }.
+
+        }
+        if($password1 != $password2){
+            $this->error('两次输入的密码不相同');
+        }
+
+        $this->reg($username,$password1);
+
+    }
+
+    private function reg($username,$password){
+        $result = Db::table('user')->where('username',$username)->find();
+
+        if($result != null){
+            $this->error('用户名已存在');
+        }
+
+        $password = md5('tp'.$password);
+
+        $data = ['username'=>$username,'password'=>$password];
+
+        Db::table('user')->insert($data);
+
+        $this->success('注册成功',url('admin/Login/index'));
+
     }
 
 
